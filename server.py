a979c63391d9a1ebcc587e986ce264284b69c6e2
sofiamaslova <sofiamaslova1985@gmail.com>
2018-10-19 20:19:18 +0700

    change 1910

--- server.py
+++ server.py
@@ -4,21 +4,36 @@ app = Flask(__name__)  # __main__
 
 @app.route('/')
 def index():
+    links = generate_links()
+    return render_template('index.html', links=links)
 
-    with app.test_request_context():
-         link = url_for('hello_user', username='Sofia the best')
+
 
 
     return render_template('index.html', link=link)
+def generate_links():
+    with app.test_request_context():
+        danil_the_great_link = url_for('hello_user', username='"Danil the great')
+        sofia_the_best_link = url_for('hello_user', username='Sofia the best')
+        index_link = url_for('index')
+        links = (
+            ("Sofia's page", sofia_the_best_link),
+            ("Danil's page", danil_the_great_link),
+            ('Index', index_link),
+        )
+
+
+    return links
+
 
 @app.route('/user/')
 @app.route('/user/<username>')
 def hello_user(username=None):
-    numbers = [0, 1, 2, 3, 4, 5, 6]
+    links = generate_links()
     return render_template(
         'userfile.html',
         username=username,
-        numbers=numbers
+        links=links,
     )
 
 
